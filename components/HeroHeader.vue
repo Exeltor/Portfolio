a9a706a298391995e783064fd798dcb133c7a82d
Alexey Zhelezov <zhelezovalexey@gmail.com>
2020-09-13 20:28:01 +0200

    links added

---
+++ components/HeroHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="h-screen/2 xl:px-56 shadow-md grid grid-cols-2">
+    <div class="my-auto">
+      <h3 class="pt-3 text-xl leading-tight">
+        Hey! My name is
+      </h3>
+      <h1 class="text-5xl font-bold text-green-400 leading-tight">
+        Alexey Zhelezov
+      </h1>
+      <p>Computer Engineering student in Madrid</p>
+      <p class="text-3xl mt-3">I will <span class="line-through text-gray-300">try to</span> solve your problems</p>
+    </div>
+    <div class="my-auto">
+      <a class="block mb-5 hover:text-green-400" href="https://github.com/Exeltor"><GithubIcon class="inline-block mr-2" />github.com/Exeltor</a>
+      <a class="block hover:text-green-400" href="https://www.linkedin.com/in/exeltor/"><LinkedinIcon class="inline-block mr-2" />linkedin.com/in/exeltor/</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class MyClass extends Vue {}
+</script>
+
+<style>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['Rubik'],
+      serif: ['Rubik'],
+      mono: ['Rubik'],
+      display: ['Rubik'],
+      body: ['Rubik']
+    },
+    extend: {
+      height: () => ({
+        'screen/2': '50vh',
+        'screen/3': 'calc(100vh / 3)',
+        'screen/4': 'calc(100vh / 4)',
+        'screen/5': 'calc(100vh / 5)'
+      })
+    }
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
+}

b38fccd7cc285a1a4555097ac325ab4f7df7c8d3
Alexey Zhelezov <zhelezovalexey@gmail.com>
2020-09-14 18:26:21 +0200

    creating a badge i dont know im not a badger

--- components/HeroHeader.vue
+++ components/HeroHeader.vue
@@ -10,9 +10,11 @@
       <p>Computer Engineering student in Madrid</p>
       <p class="text-3xl mt-3">I will <span class="line-through text-gray-300">try to</span> solve your problems</p>
     </div>
-    <div class="my-auto">
-      <a class="block mb-5 hover:text-green-400" href="https://github.com/Exeltor"><GithubIcon class="inline-block mr-2" />github.com/Exeltor</a>
-      <a class="block hover:text-green-400" href="https://www.linkedin.com/in/exeltor/"><LinkedinIcon class="inline-block mr-2" />linkedin.com/in/exeltor/</a>
+    <div class="flex flex-col">
+      <img class="rounded object-cover h-64" src="@/assets/self-picture.jpg" alt="Alexey Zhelezov">
+      <!-- <LinkedinIcon />
+      <GithubIcon /> -->
+      <Badge image-path=".../.../assets/checkmark.svg" text="Trial badge" />
     </div>
   </div>
 </template>

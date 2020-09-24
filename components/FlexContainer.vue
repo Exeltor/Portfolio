996586444efea946f3ca220e4887ea0839c6a919
Alexey Zhelezov <zhelezovalexey@gmail.com>
2020-09-24 21:57:37 +0200

    building switcher

---
+++ components/FlexContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col items-center text-center lg:text-left lg:flex-row border-2 border-gray-800 rounded p-5 mb-5">
+    <slot name="feature-content" />
+    <div class="break-word">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class FlexContainer extends Vue {}
+</script>
+
+<style lang="postcss">
+</style>
--- pages/skills.vue
+++ pages/skills.vue
@@ -5,14 +5,21 @@
         My Skills
       </h1>
     </div>
+    <div class="inline-block border rounded mx-auto">
+      <button class="border-r">FullStack</button>
+      <button class="border-r">Mobile</button>
+      <button>Personal Qualities</button>
+    </div>
     <div class="grid lg:grid-cols-2 sm:grid-cols-1">
       <div>
         <h2 class="text-3xl font-bold leading-tight text-center mb-5">
           Frontend
         </h2>
-        <div class="flex flex-col items-center text-center lg:text-left lg:flex-row border-2 border-gray-800 rounded p-5 mb-5">
-          <img src="@/assets/nuxt-logo.svg" alt="Nuxt" class="w-32 lg:mr-3">
-          <div class="break-word">
+        <FlexContainer>
+          <template #feature-content>
+            <img src="@/assets/nuxt-logo.svg" alt="Nuxt" class="w-32 lg:mr-3">
+          </template>
+          <template #content>
             <h3 class="text-3xl font-bold leading-tight">
               Nuxt.js
             </h3>
@@ -23,11 +30,13 @@
               I use it extensively for many projects
             </p>
             <small>Psst... This portfolio is made and statically generated with Nuxt...</small>
-          </div>
-        </div>
-        <div class="flex flex-col items-center text-center lg:text-left lg:flex-row border-2 border-gray-800 rounded p-5 mb-5">
-          <img src="@/assets/vue-logo.png" alt="Vue" class="w-32 lg:mr-3">
-          <div class="break-word">
+          </template>
+        </FlexContainer>
+        <FlexContainer>
+          <template #feature-content>
+            <img src="@/assets/vue-logo.png" alt="Vue" class="w-32 lg:mr-3">
+          </template>
+          <template #content>
             <h3 class="text-3xl font-bold leading-tight">
               Vue
             </h3>
@@ -35,11 +44,13 @@
             <p>
               Learned it and quickly switched to Nuxt.js
             </p>
-          </div>
-        </div>
-        <div class="flex flex-col items-center text-center lg:text-left lg:flex-row border-2 border-gray-800 rounded p-5">
-          <img src="@/assets/angular-logo.svg" alt="Angular" class="w-32 lg:mr-3">
-          <div class="break-word">
+          </template>
+        </FlexContainer>
+        <FlexContainer>
+          <template #feature-content>
+            <img src="@/assets/angular-logo.svg" alt="Angular" class="w-32 lg:mr-3">
+          </template>
+          <template #content>
             <h3 class="text-3xl font-bold leading-tight">
               Angular
             </h3>
@@ -49,8 +60,8 @@
               <br>
               This is the first framework I learned and I have developed multiple projects with it
             </p>
-          </div>
-        </div>
+          </template>
+        </FlexContainer>
       </div>
       <div>
         <h2 class="text-3xl font-bold leading-tight text-center">
